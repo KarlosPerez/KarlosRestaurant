d6106502e884583c436e519934c3231d4e7ff3cd
Carlos Andres <legomarth@hotmail.com>
2019-06-24 16:26:10 -0500

    Feature: import facebook account kit and generate Hash_Key

--- app/src/main/java/com/karlosprojects/androidkarlosrestaurant/SplashScreen.java
+++ app/src/main/java/com/karlosprojects/androidkarlosrestaurant/SplashScreen.java
@@ -3,8 +3,16 @@ package com.karlosprojects.androidkarlosrestaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -18,5 +26,8 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
             }
         }, 3000);
+
+
     }
+
 }
