d8fe6b042f5441ac3cc9f6d8a6d43c66ea136443
Carlos Andres <legomarth@hotmail.com>
2019-06-24 20:33:24 -0500

    Feature: implement authentication by phone, with facebook account kit.

--- app/src/main/java/com/karlosprojects/androidkarlosrestaurant/MainActivity.java
+++ app/src/main/java/com/karlosprojects/androidkarlosrestaurant/MainActivity.java
@@ -1,14 +1,63 @@
 package com.karlosprojects.androidkarlosrestaurant;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+import android.accounts.Account;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.LoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int APP_REQUEST_CODE = 1234;
+    @BindView(R.id.btn_sign_in)
+    Button btn_sign_in;
+
+    @OnClick(R.id.btn_sign_in)
+    void loginUser() {
+        Intent intent = new Intent(this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder builder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
+                        AccountKitActivity.ResponseType.TOKEN);
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                builder.build());
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == APP_REQUEST_CODE) {
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            String toastMessage;
+            if(loginResult.getError() != null) {
+                toastMessage = loginResult.getError().getErrorType().getMessage();
+                Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
+            } else if (loginResult.wasCancelled()) {
+                toastMessage = "Login Cancelled";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Success Login", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
