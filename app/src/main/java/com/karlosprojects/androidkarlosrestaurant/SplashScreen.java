0d196ebc0d2f34ea6c3c7255def283486c69c6a7
Carlos Andres <legomarth@hotmail.com>
2019-06-24 20:49:17 -0500

    Feature: ask for permissions using Dexter, and handle session with logged account.

--- app/src/main/java/com/karlosprojects/androidkarlosrestaurant/SplashScreen.java
+++ app/src/main/java/com/karlosprojects/androidkarlosrestaurant/SplashScreen.java
@@ -2,6 +2,7 @@ package com.karlosprojects.androidkarlosrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -10,6 +11,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -20,12 +33,44 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new Handler().postDelayed(new Runnable() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                            @Override
+                            public void onSuccess(Account account) {
+                                Toast.makeText(SplashScreen.this, "Already Logged", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onError(AccountKitError accountKitError) {
+                                Toast.makeText(SplashScreen.this, "Not Sign in! Please sign in", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                                finish();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(SplashScreen.this, "You must accept this permission to use our app", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+
+        /*new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
             }
-        }, 3000);
+        }, 3000);*/
 
 
     }
