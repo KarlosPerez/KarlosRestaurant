93e6e4ac01942e139999cac74380ef416524de63
Carlos Andres <legomarth@hotmail.com>
2019-06-28 00:41:58 -0500

    Feature: implement some logic on MainActivity without MVP. Needs refactor to MVP

--- app/src/main/java/com/karlosprojects/androidkarlosrestaurant/MainActivity/View/MainActivity.java
+++ app/src/main/java/com/karlosprojects/androidkarlosrestaurant/MainActivity/View/MainActivity.java
@@ -5,21 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import dmax.dialog.SpotsDialog;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
-import android.accounts.Account;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
 import com.facebook.accountkit.AccountKitLoginResult;
-import com.facebook.accountkit.LoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
+import com.karlosprojects.androidkarlosrestaurant.HomeActivity.view.HomeActivity;
+import com.karlosprojects.androidkarlosrestaurant.MainActivity.Model.UserModel;
 import com.karlosprojects.androidkarlosrestaurant.R;
+import com.karlosprojects.androidkarlosrestaurant.Retrofit.IRestaurantAPI;
+import com.karlosprojects.androidkarlosrestaurant.Retrofit.RetrofitClient;
+import com.karlosprojects.androidkarlosrestaurant.UpdateInfoActivity.view.UpdateInfoActivity;
+import com.karlosprojects.androidkarlosrestaurant.Utils.Common;
 
 public class MainActivity extends AppCompatActivity implements MainActivityView {
+
+    IRestaurantAPI iRestaurantAPI;
+    CompositeDisposable compositeDisposable;
+    AlertDialog dialog;
 
     private static final int APP_REQUEST_CODE = 1234;
     @BindView(R.id.btn_sign_in)
@@ -37,6 +55,12 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
     }
 
     @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == APP_REQUEST_CODE) {
@@ -49,7 +73,40 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
                 toastMessage = "Login Cancelled";
                 Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(this, "Success Login", Toast.LENGTH_SHORT).show();
+                dialog.show();
+                AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                    @Override
+                    public void onSuccess(Account account) {
+                        compositeDisposable.add(iRestaurantAPI.getUser(Common.API_KEY, account.getId())
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Consumer<UserModel>() {
+                            @Override
+                            public void accept(UserModel userModel) throws Exception {
+                                //if user already in database
+                                if(userModel.isSuccess()) {
+                                    Common.currentUser = userModel.getResult().get(0);
+                                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                                    finish();
+                                } else { //if user not registered
+                                    startActivity(new Intent(MainActivity.this, UpdateInfoActivity.class));
+                                    finish();
+                                }
+                            }
+                        }, new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+                                dialog.dismiss();
+                                Toast.makeText(MainActivity.this, "[GET USER]"+throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }));
+                    }
+
+                    @Override
+                    public void onError(AccountKitError accountKitError) {
+                        Toast.makeText(MainActivity.this, "[ACCOUNT KIT ERROR]"+ accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         }
     }
@@ -58,6 +115,19 @@ public class MainActivity extends AppCompatActivity implements MainActivityView 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        initAPI();
+    }
+
+    private void init() {
         ButterKnife.bind(this);
+        dialog = new SpotsDialog.Builder()
+                .setCancelable(false)
+                .setContext(this)
+                .build();
+    }
+
+    private void initAPI() {
+        iRestaurantAPI = RetrofitClient.getInstance(Common.API_RESTAURANT_ENDPOINT).create(IRestaurantAPI.class);
     }
 }
