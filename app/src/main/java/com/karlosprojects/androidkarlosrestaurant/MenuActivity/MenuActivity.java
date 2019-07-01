a9820eed5ad3572d836b9508ae211c51a604593e
Carlos Andres <legomarth@hotmail.com>
2019-07-01 00:42:58 -0500

    Feature: create menu activity

---
+++ app/src/main/java/com/karlosprojects/androidkarlosrestaurant/MenuActivity/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.karlosprojects.androidkarlosrestaurant.MenuActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.karlosprojects.androidkarlosrestaurant.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
