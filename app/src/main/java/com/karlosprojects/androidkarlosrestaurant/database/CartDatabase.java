010b4c735d8547555d112ea895d382d5af55e76a
Carlos Andres <legomarth@hotmail.com>
2019-07-21 14:17:51 -0500

    Feature: create CartDatabase using Room

---
+++ app/src/main/java/com/karlosprojects/androidkarlosrestaurant/database/CartDatabase.java
@@ -0,0 +1,20 @@
+package com.karlosprojects.androidkarlosrestaurant.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(version = 1, entities = CartItem.class, exportSchema = false)
+public abstract class CartDatabase extends RoomDatabase {
+    private static CartDatabase instance;
+
+    public static CartDatabase getInstance(Context context) {
+        if (instance == null)
+            instance = Room.databaseBuilder(context, CartDatabase.class, "KarlosRestaurantCart")
+                    .build();
+        return instance;
+    }
+
+}
